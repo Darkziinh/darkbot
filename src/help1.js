6c79dd4f6c4748c6c10e66a76a10f60a52cb63a8
Lucas617240 <77517852+Lucas617240@users.noreply.github.com>
2021-01-16 19:54:27 -0300

    Add files via upload

---
+++ src/help1.js
@@ -0,0 +1,45 @@
+const help1 = (prefix) => {
+
+	return `
+╭──────────────────╮
+ *COMANDOS DO Lucasss*
+╰──────────────────╯
+ 
+➸ *${prefix}marcar*
+➸ *${prefix}marcar2*
+➸ *${prefix}marcar3*
+➸ *${prefix}loli*
+➸ *${prefix}loli1*
+➸ *${prefix}hentai*
+➸ *${prefix}dono*
+➸ *${prefix}porno*
+➸ *${prefix}boanoite*
+➸ *${prefix}bomdia*
+➸ *${prefix}boatarde*
+➸ *${prefix}mia*
+➸ *${prefix}mia1*
+➸ *${prefix}mia2*
+➸ *${prefix}belle*
+➸ *${prefix}belle1*
+➸ *${prefix}belle2*
+➸ *${prefix}belle3*
+➸ *${prefix}akeno*
+➸ *${prefix}meme*
+➸ *${prefix}lofi*
+➸ *${prefix}malkova*
+➸ *${prefix}canal*
+➸ *${prefix}nsfwloli*
+➸ *${prefix}reislin*
+➸ *${prefix}limpar*
+➸ *${prefix}marcar*
+➸ *${prefix}ts (texto que deseja transmitir)*
+
+════════════════════
+*Lucasss* 🤗
+*Digite ${prefix}dono para mais info*
+════════════════════`
+
+}
+exports.help1 = help1
+
+
